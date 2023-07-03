3d97f554878f8a33d311602718cb8d1b4a0918de
munnep <patrick.munne@hashicorp.com>
2023-07-03 16:04:32 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
+
+
+resource "null_resource" "test" {}
