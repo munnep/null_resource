14b6af8fc92acb13b1e05de6adc05ffaedccf945
munnep <patrick.munne@hashicorp.com>
2023-11-29 15:52:05 +0100

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -1,1 +1,14 @@
-fff
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
+
+resource "null_resource" "test" {}
