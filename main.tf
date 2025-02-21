52107a9ad578d8caa15565fd1e2f3fd6241decb2
munnep <patrick.munne@hashicorp.com>
2025-02-21 13:37:09 +0100

    tesrte8

--- main.tf
+++ main.tf
@@ -6,4 +6,5 @@ resource "null_resource" "test3" {}
 resource "null_resource" "test4" {}
 resource "null_resource" "test5" {}
 resource "null_resource" "test6" {}
-resource "null_resource" "test6" {}
+resource "null_resource" "test7" {}
+resource "null_resource" "test8" {}
