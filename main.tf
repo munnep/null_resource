5ef191fb08103ff8d40ecd1eeafc4f50bc4eae6d
munnep <patrick.munne@hashicorp.com>
2025-04-23 09:59:17 +0200

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,2 @@
-resource "null_resource" "previous" {}
+resource "null_resource" "test" {}
 
-resource "time_sleep" "wait" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "10s"
-}
-
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait]
-}
