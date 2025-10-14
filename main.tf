8dcffe66b7a697569a0356c6644854cc63cdfda6
munnep <patrick.munne@hashicorp.com>
2025-10-14 12:38:22 +0200

    test3

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 data "external" "slow_delay" {
   program = ["bash", "-c", <<EOT
-    sleep 51
+    sleep 4
     echo '{ "result": "done" }'
 EOT
   ]
 }
 
-resource "null_resource" "test2" {}
+resource "null_resource" "test3" {}
 
 output "delay_result" {
   value = data.external.slow_delay.result
